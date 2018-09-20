b1df7da4572b312f949ce193fcc4f7ad550a1e42
tylerthomasallen <tylerthomasallen@gmail.com>
2018-09-19 17:29:10 -0700

    start on cookie

--- skeleton/lib/session.rb
+++ skeleton/lib/session.rb
@@ -3,10 +3,19 @@ require 'json'
 class Session
   # find the cookie for this app
   # deserialize the cookie into a hash
-  def initialize(req)
+  def initialize(req)# req.path
+    # {'_rails_lite_app' => { 'xyz' => 'abc' }
+    if req.cookies['_rails_lite_app']
+      @cookie = req.cookies['_rails_lite_app']
+    else
+      @cookie = req.cookies['_rails_lite_app'] = {}
+    end
   end
 
+# =>  rails_lite_app[]
+
   def [](key)
+    JSON.parse(@cookie)[key]
   end
 
   def []=(key, val)
@@ -15,5 +24,6 @@ class Session
   # serialize the hash into json and save in a cookie
   # add to the responses cookies
   def store_session(res)
+    res.set_cookie
   end
 end
